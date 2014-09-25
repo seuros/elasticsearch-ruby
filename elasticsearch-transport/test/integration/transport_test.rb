f0abc1c385f1e316ff24ed43127a1452e1c0a1ed
Abdelkader Boudih <terminale@gmail.com>
2014-09-25 08:35:40 +0000

    fix test

--- elasticsearch-transport/test/integration/transport_test.rb
+++ elasticsearch-transport/test/integration/transport_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class Elasticsearch::Transport::ClientIntegrationTest < ElasticsearchMiniTest::IntegrationTestCase
+class Elasticsearch::Transport::ClientIntegrationTest < Elasticsearch::Test::IntegrationTestCase
   startup do
     Elasticsearch::TestCluster.start if ENV['SERVER'] and not Elasticsearch::TestCluster.running?
   end
